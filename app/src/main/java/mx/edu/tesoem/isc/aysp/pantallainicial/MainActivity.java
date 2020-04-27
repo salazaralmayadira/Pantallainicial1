6ffba38ba8eb79d0681114e4e8789d1b4940b3cc
salazaralmayadira <64059447+salazaralmayadira@users.noreply.github.com>
2020-04-26 22:33:33 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/aysp/pantallainicial/MainActivity.java
@@ -0,0 +1,27 @@
+package mx.edu.tesoem.isc.aysp.pantallainicial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent= new Intent(MainActivity.this,PrincipalActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 10000);
+    }
+}
---
+++ app/src/main/java/mx/edu/tesoem/isc/aysp/pantallainicial/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.aysp.pantallainicial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
